42edea6a2f515f876fdb5a8ea7b2a83abbf88f49
BrandonChung <cto@gavrint.com>
2020-03-06 22:15:28 +0900

    master

---
+++ R2FLauncher/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace R2FLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(36, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(731, 299);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(731, 299);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://cafe.naver.com/f4u2", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Algerian", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(36, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "시작하기";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Algerian", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(628, 440);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "닫기";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Algerian", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ R2FLauncher/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace R2FLauncher
+{
+    public partial class Form1 : Form
+    {
+        String encrypt_string = "GodAlwaysLovesUs!ThanksToGod!";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool ok_or_not = false;
+
+            if ( DownloadAndDecryption( "http://r2f.netmego.com:8812/Bundles/Patch/Managed/Assembly-CSharp.unity3d", "./R2Fantasy_Data/Managed/Assembly-CSharp.dll" ) )
+                if ( DownloadAndDecryption( "http://r2f.netmego.com:8812/Bundles/Patch/Managed/Assembly-CSharp-firstpass.unity3d", "./R2Fantasy_Data/Managed/Assembly-CSharp-firstpass.dll" ) )
+                    if ( DownloadAndDecryption( "http://r2f.netmego.com:8812/Bundles/Patch/Managed/Assembly-UnityScript.unity3d", "./R2Fantasy_Data/Managed/Assembly-UnityScript.dll" ) )
+                        if ( DownloadAndDecryption( "http://r2f.netmego.com:8812/Bundles/Patch/Managed/Assembly-UnityScript-firstpass.unity3d", "./R2Fantasy_Data/Managed/Assembly-UnityScript-firstpass.dll" ) )
+                            ok_or_not = true;
+
+            if ( !ok_or_not )
+            {
+                File.Delete( "./R2Fantasy_Data/Managed/Assembly-CSharp.dll" );
+                File.Delete( "./R2Fantasy_Data/Managed/Assembly-CSharp-firstpass.dll" );
+                File.Delete( "./R2Fantasy_Data/Managed/Assembly-UnityScript.dll" );
+                File.Delete( "./R2Fantasy_Data/Managed/Assembly-UnityScript-firstpass.dll" );
+                Application.Exit();
+            }
+            else
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(".\\R2Fantasy.exe");
+                startInfo.Arguments = "-launchfromlauncher";
+                Process.Start(startInfo);
+                Application.Exit();
+            }
+
+        }
+
+        bool DownloadAndDecryption(String urlpath, String localpath)
+        {
+            WebClient webClient = new WebClient();
+            byte[] received = null;
+            try
+            {
+                received = webClient.DownloadData(urlpath);
+            }
+            catch(WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (received == null)
+                return false;
+
+            UTF8Encoding encoding = new UTF8Encoding();
+            byte[] encryptcode = encoding.GetBytes(encrypt_string);
+
+            for( int i = 0; i < received.Length; i++ )
+                received[i] ^= encryptcode[i%encryptcode.Length];
+
+            File.WriteAllBytes(localpath, received);
+
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
